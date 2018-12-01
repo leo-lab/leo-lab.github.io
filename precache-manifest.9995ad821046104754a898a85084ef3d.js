55f9d53945e7cc02765884bdb1f444890b7dcc23
hojin <hojin@wisefour.com>
2018-12-01 21:31:57 +0900

    Init!

---
+++ precache-manifest.9995ad821046104754a898a85084ef3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8de9d1cbcdaae3c39e4b",
+    "url": "/static/js/main.8de9d1cb.chunk.js"
+  },
+  {
+    "revision": "e0d67f13ccf858b86c5c",
+    "url": "/static/js/1.e0d67f13.chunk.js"
+  },
+  {
+    "revision": "8de9d1cbcdaae3c39e4b",
+    "url": "/static/css/main.1523ed73.chunk.css"
+  },
+  {
+    "revision": "e0d67f13ccf858b86c5c",
+    "url": "/static/css/1.0bc9ec88.chunk.css"
+  },
+  {
+    "revision": "4c2275f8b3699083080975f65f575a0b",
+    "url": "/index.html"
+  }
+];
